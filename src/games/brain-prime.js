2921f6151b6e19c2df312b9ac3ade04decfa0a4d
Proggit <proggit@mail.ru>
2021-02-13 22:59:54 +0500

    Add isPrime game

---
+++ src/games/brain-prime.js
@@ -0,0 +1,27 @@
+import gameEngine from '../index.js';
+import randomDigit from '../libs/random.js';
+
+const isPrime = (digit) => {
+  let count = 0;
+  for (let i = 1; i <= digit; i += 1) {
+    if (digit % i === 0) {
+      count += 1;
+    }
+    if (count > 2) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gameInPrime = () => {
+  const randDigit = randomDigit(1, 10);
+  const resultRandEven = isPrime(randDigit);
+  return {
+    question: randDigit,
+    answer: resultRandEven,
+  };
+};
+
+export default () => gameEngine(gameInPrime, description);
